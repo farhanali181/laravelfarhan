827c0c80541a302023f70c372c66035e4283f565
farhanali181 <089669442684fa@gmail.com>
2021-05-22 15:00:45 -0700

    Tugas Praktikum

---
+++ resources/views/artikel.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Artikel</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="/artikel">Artikel</a>
+        <a class="nav-link" href="/contactus">Contact Us</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    <h3>Apa Itu Framework?
+Framework adalah kerangka kerja yang digunakan untuk mengembangkan aplikasi berbasis desktop atau aplikasi berbasis website. Dengan menggunakan framework Anda akan lebih mudah untuk membuat aplikasi atau website. Itu karena Anda hanya perlu menyusun komponen-komponen pemrograman yang sudah jadi. Dengan kata lain, Anda tidak perlu membuat berbagai fitur dari awal lagi. 
+Framework sengaja diciptakan untuk membantu developer mengembangkan aplikasi atau website lebih cepat serta tersusun dan terstruktur. 
+
+Dengan begitu, developer dan programmer tidak perlu melakukan koding program yang diulang-ulang.
+
+Nah, sudah tahu kan apa itu framework? Sekarang, mari pelajari fungsinya.
+
+Fungsi Framework
+Framework memiliki fungsi utama untuk mempermudah para developer mengembangkan aplikasi dan website terkait struktur MVC (Model View Controller) yang digunakan. Selain itu, ada tiga fungsi framework yang dapat Anda ketahui di bawah ini:
+
+1. Program Menjadi Lebih Terstruktur dan Tersusun
+Saat developer mengerjakan pengembangan aplikasi atau website yang besar, maka program yang akan ditulis di dalamnya menjadi semakin banyak. Terkadang, semakin banyaknya program yang ditulis akan menjadikan proses debugging semakin lambat. Selain itu, saat Anda mencari kode program yang error akan semakin sulit karena program yang tidak terstruktur.
+
+Oleh karena itu, dengan adanya framework dapat menjadikan program menjadi lebih terstruktur sehingga Anda dapat dengan mudah menemukan kode yang perlu diperbaiki. Bahkan beberapa jenis framework ada yang menerapkan konsep MVC (Model View Controller) yang akan memudahkan developer untuk memisahkan antara logika dan view.
+
+2. Praktis untuk Developer
+Saat Anda mengembangkan aplikasi atau website, akan lebih praktis ketika Anda menggunakan framework. Alasannya karena framework sudah menyediakan kode berupa function dan class jadi proses pembuatan software atau aplikasi akan menjadi lebih cepat. Anda cukup memanggil function atau class tersebut ke dalam kode program Anda.</h3>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
